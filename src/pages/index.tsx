39775e9df4337455fad389a34c9f39c6466bc28e
gustavo falcione <falcionegustavo@gmail.com>
2022-05-10 17:47:43 -0300

    refatorando

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import Image from 'next/image';
 import avatarSvg from '../../public/images/avatar.svg'
@@ -6,6 +6,10 @@ import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss'
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -36,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps  = async () => {
+export const getStaticProps: GetStaticProps  = async () => {
   const price = await stripe.prices.retrieve('price_1Kxb0aBWVmHxutkw6hF7HeYE')
 
   const product = {
@@ -50,6 +54,7 @@ export const getServerSideProps: GetServerSideProps  = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
