99c08da02e34a87868fa25c8ae6dc58b3d0517f2
gustavo falcione <falcionegustavo@gmail.com>
2022-05-16 10:11:48 -0300

    Configurando navegacao do menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,25 @@
 import Image from 'next/image';
 import logoSvg from '../../../public/images/logo.svg'
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
 
 import styles from './styles.module.scss'
 
 export function Header() {
+  
+
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src={logoSvg} alt="ignews your technology blog" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
