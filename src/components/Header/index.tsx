4efa8f12ccfa811893df9a8c2d8042054a144f18
Gustavo Falcione Cagnato <falcionegustavo@gmail.com>
2023-03-28 08:43:38 -0300

    Implementando testes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,30 @@
-import Image from 'next/image';
-import logoSvg from '../../../public/images/logo.svg'
-import { SignInButton } from '../SignInButton';
-import { ActiveLink } from '../ActiveLink';
+import Image from "next/image";
+import { SignInButton } from "../SignInButton";
+import { ActiveLink } from "../ActiveLink";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export function Header() {
-  
-
-  return(
+  return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src={logoSvg} alt="ignews your technology blog" />
+        <Image
+          src="/images/logo.svg"
+          alt="ignews your technology blog"
+          width="110" 
+          height="31"
+        />
         <nav>
-          <ActiveLink activeClassName={styles.active} href='/'>
+          <ActiveLink activeClassName={styles.active} href="/">
             <a>Home</a>
           </ActiveLink>
 
-          <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
           </ActiveLink>
         </nav>
         <SignInButton />
       </div>
     </header>
-  )
+  );
 }
---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react'
+import { useSession } from 'next-auth/react'
+import { SignInButton } from '.'
+
+jest.mock('next-auth/react')
+
+describe('SignInButton component', () => {
+  
+  it('renders correctly when user is not authenticated', () => {
+
+   const useSessionMocked = jest.mocked(useSession);
+
+   useSessionMocked.mockReturnValueOnce({ data: null , status: 'unauthenticated' })
+
+    const { debug } = render(
+      <SignInButton />
+    )
+
+    debug() // o debug serve como uma espécie de console.log... Mostrará todo o html do componente
+  
+    expect(screen.getByText('Sign in with Github')).toBeInTheDocument()
+  })
+
+
+  it('renders correctly when user is authenticated', () => {
+    render(
+      <SignInButton />
+    )
+  
+    expect(screen.getByText('John Doe')).toBeInTheDocument()
+  })
+  
+})
